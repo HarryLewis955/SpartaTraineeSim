86b4a336b7ccd2669d8af4ca4b0c4750dd490e0f
mehmetyetim <yetimmeh@gmail.com>
2022-02-15 17:25:59 +0000

    TrainingCentreStepDefinitions.java added

---
+++ src/test/java/com/sparta/simulation/stepdefs/TrainingCentreStepDefinitions.java
@@ -0,0 +1,63 @@
+package com.sparta.simulation.stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TrainingCentreStepDefinitions {
+    @Given("user enters {int} to start simulation")
+    public void userEntersToStartSimulation(int arg0) {
+    }
+
+    @When("the program is generating {int} trainees in this month")
+    public void theProgramIsGeneratingTraineesInThisMonth(int arg0) {
+    }
+
+    @When("no bootcamp opened")
+    public void noBootcampOpened() {
+    }
+
+    @When("no tech centre opened")
+    public void noTechCentreOpened() {
+    }
+
+    @Then("maximum {int} training hubs should be opened")
+    public void maximumTrainingHubsShouldBeOpened(int arg0) {
+    }
+
+    @Then("minimum {int} training hub should be opened")
+    public void minimumTrainingHubShouldBeOpened(int arg0) {
+    }
+
+    @When("{int} training hub opened")
+    public void trainingHubOpened(int arg0) {
+    }
+
+    @Then("no training hub opened")
+    public void noTrainingHubOpened() {
+    }
+
+    @Then("simulation should be ended")
+    public void simulationShouldBeEnded() {
+    }
+
+    @Then("training hub {int} is full of {int} trainees")
+    public void trainingHubIsFullOfTrainees(int arg0, int arg1) {
+    }
+
+    @When("the program is generating {int} of trainees in this month")
+    public void theProgramIsGeneratingOfTraineesInThisMonth(int arg0) {
+    }
+
+    @Then("{int} bootcamp should be opened")
+    public void BootcampShouldBeOpened() {
+    }
+
+    @Then("training hub {int} takes new {int} trainees")
+    public void trainingHubTakesNewTrainees(int arg0, int arg1) {
+    }
+
+    @When("bootcamp {int} takes {int} new trainees")
+    public void bootcampTakesNewTrainees(int arg0, int arg1) {
+    }
+}
