db646b772373a351b1a031f3dd6b451adc293bec
BwongmcST <ben.manchunwong@gmail.com>
2022-02-18 18:50:12 +0000

    edited test method

--- src/test/java/com/sparta/simulation/test/centrecontrollertest/CentreControllerAddToCentreTest.java
+++ src/test/java/com/sparta/simulation/test/centrecontrollertest/CentreControllerAddToCentreTest.java
@@ -22,8 +22,10 @@ public class CentreControllerAddToCentreTest {
     int dataCount;
     int devopsCount;
     int businessCount;
-
+    int[] monthlyTrainees;
+    ArrayList<int[]> traineesInTraining;
     int idCount;
+    int month;
 
     @BeforeEach
     void setUp() {
@@ -37,16 +39,19 @@ public class CentreControllerAddToCentreTest {
         businessCount = 0;
         newTrainees = 0;
         idCount = 0;
+        monthlyTrainees = new int[6];
+        traineesInTraining = new ArrayList<>();
+        month =0;
     }
 
     @Test
     @DisplayName("add 5 trainees to Boot Camp with 5 capacity expect all added into Boot Camp and waiting list should be empty")
     public void add5TraineesToBootCampWith5CapacityExpectAllAddedIntoHubAndEmptyWaitingList(){
         newTrainees = 5;
-        waitingList = TraineeController.createTrainee(newTrainees, javaCount,csharpCount,dataCount,devopsCount,businessCount,waitingList);
+        int[] monthlyCount = TraineeController.createTrainee(newTrainees, javaCount,csharpCount,dataCount,devopsCount,businessCount,waitingList);
 
         centreController.createCentre(centreList, 5, 2,idCount,0,0);
-        centreController.addToCentre(waitingList, centreList, waitingList.size());
+        centreController.addToCentre(waitingList, centreList, waitingList.size(),monthlyTrainees,traineesInTraining,month);
 
         Assertions.assertEquals(5,                                                          //Check 5 students added into the camp
                 centreList.get(0).getJavaCount()
@@ -63,10 +68,10 @@ public class CentreControllerAddToCentreTest {
     @DisplayName("add 6 trainees to Boot Camp with 5 capacity expect 5 added into Boot Camp and waiting list should be 1")
     public void add6TraineesToBootCampWith5CapacityExpect5AddedIntoHubAnd1StudentInWaitingList(){
         newTrainees = 6;
-        waitingList = TraineeController.createTrainee(newTrainees, javaCount,csharpCount,dataCount,devopsCount,businessCount,waitingList);
+        int[] monthlyCount = TraineeController.createTrainee(newTrainees, javaCount,csharpCount,dataCount,devopsCount,businessCount,waitingList);
 
         centreController.createCentre(centreList, 5, 2,idCount,0,0);
-        centreController.addToCentre(waitingList, centreList, waitingList.size());
+        centreController.addToCentre(waitingList, centreList, waitingList.size(),monthlyTrainees,traineesInTraining,month);
 
         Assertions.assertEquals(5,                                                          //Check 5 students added into the camp
                 centreList.get(0).getJavaCount()
@@ -80,14 +85,14 @@ public class CentreControllerAddToCentreTest {
     }
 
     @Test
-    @DisplayName("add 7 trainees to 2 boot camp with total capacity expect all added into Boot Camp and waiting list should be empty")
+    @DisplayName("add 7 trainees to 2 boot camp with total 7 capacity expect all added into Boot Camp and waiting list should be empty")
     public void add7TraineesTo2BootCampWithTotal7CapacityExpect5AddedIntoHubAndWaitingListEmpty(){
         newTrainees = 7;
-        waitingList = TraineeController.createTrainee(newTrainees, javaCount,csharpCount,dataCount,devopsCount,businessCount,waitingList);
+        int[] monthlyCount = TraineeController.createTrainee(newTrainees, javaCount,csharpCount,dataCount,devopsCount,businessCount,waitingList);
 
         idCount = centreController.createCentre(centreList, 2, 2,idCount,0,0);
         idCount = centreController.createCentre(centreList, 5, 2,idCount,0,0);
-        centreController.addToCentre(waitingList, centreList, waitingList.size());
+        centreController.addToCentre(waitingList, centreList, waitingList.size(),monthlyTrainees,traineesInTraining,month);
 
         Assertions.assertEquals(newTrainees, (centreList.get(0).getTotal()+ centreList.get(1).getTotal()));
 
@@ -99,10 +104,10 @@ public class CentreControllerAddToCentreTest {
     @DisplayName("add 50 trainees to 1 training hub with random 0 - 50 capacity expect hub full filled and rest will be on waiting list")
     public void add50TraineesTo1TrainingHubWith0To50CapacityExpectHubFullAndRestWillBeOnWaitingList(){
         newTrainees = 50;
-        waitingList = TraineeController.createTrainee(newTrainees, javaCount,csharpCount,dataCount,devopsCount,businessCount,waitingList);
+        int[] monthlyCount = TraineeController.createTrainee(newTrainees, javaCount,csharpCount,dataCount,devopsCount,businessCount,waitingList);
 
         idCount = centreController.createCentre(centreList, 0, 1,idCount,1,0);      //random capacity for centreType 1
-        centreController.addToCentre(waitingList, centreList, waitingList.size());
+        centreController.addToCentre(waitingList, centreList, waitingList.size(),monthlyTrainees,traineesInTraining,month);
 
 
         Assertions.assertEquals(centreList.get(0).getCentreCapacity(),
@@ -113,60 +118,25 @@ public class CentreControllerAddToCentreTest {
                         + centreList.get(0).getBusinessCount());
 
         Assertions.assertEquals(newTrainees - centreList.get(0).getCentreCapacity(), waitingList.size());
+        //System.out.println("Hub Capacity: " + centreList.get(0).getCentreCapacity());
+        //System.out.println("Waiting List Size: " + waitingList.size());
     }
 
     @Test
     @DisplayName("add 50 trainees to 2 training hub with random 0 - 50 capacity expect hub full filled and rest will be on waiting list")
     public void add50TraineesTo2TrainingHubWith0To50CapacityExpectHubFullAndRestWillBeOnWaitingList(){
         newTrainees = 50;
-        waitingList = TraineeController.createTrainee(newTrainees, javaCount,csharpCount,dataCount,devopsCount,businessCount,waitingList);
+        int[] monthlyCount = TraineeController.createTrainee(newTrainees, javaCount,csharpCount,dataCount,devopsCount,businessCount,waitingList);
 
         idCount = centreController.createCentre(centreList, 0, 1,idCount,2,0);      //random capacity 0-50 (inside method) for centreType 1
-        centreController.addToCentre(waitingList, centreList, waitingList.size());
+        centreController.addToCentre(waitingList, centreList, waitingList.size(),monthlyTrainees,traineesInTraining,month);
 
-
-        Assertions.assertEquals(centreList.get(0).getCentreCapacity(),
-                centreList.get(0).getJavaCount()
-                        + centreList.get(0).getCsharpCount()
-                        + centreList.get(0).getDataCount()
-                        + centreList.get(0).getDevopsCount()
-                        + centreList.get(0).getBusinessCount());
-
-        int numberOfStudentsLeftAfterFirstLoop = newTrainees - centreList.get(0).getCentreCapacity();
-        int expectedStudentsInSecondHub;
-        boolean isStudentLeft;
-
-        if(numberOfStudentsLeftAfterFirstLoop >= centreList.get(1).getCentreCapacity()){
-            expectedStudentsInSecondHub = centreList.get(1).getCentreCapacity();
-            isStudentLeft = true;
-        }else{
-            expectedStudentsInSecondHub =  numberOfStudentsLeftAfterFirstLoop;
-            isStudentLeft = false;
-        }
-
-        Assertions.assertEquals(expectedStudentsInSecondHub,
-                centreList.get(1).getJavaCount()
-                        + centreList.get(1).getCsharpCount()
-                        + centreList.get(1).getDataCount()
-                        + centreList.get(1).getDevopsCount()
-                        + centreList.get(1).getBusinessCount());
-
-        if(isStudentLeft == true) {
-            Assertions.assertEquals(newTrainees - centreList.get(0).getCentreCapacity() - expectedStudentsInSecondHub, waitingList.size());
+        if(newTrainees >= centreList.get(0).getCentreCapacity() + centreList.get(1).getCentreCapacity()){
+            Assertions.assertEquals(waitingList.size(), newTrainees - centreList.get(0).getCentreCapacity() - centreList.get(1).getCentreCapacity());
         }else{
             Assertions.assertEquals(0, waitingList.size());
         }
-
-        System.out.println("Center 1 students: " + centreList.get(0).getCentreCapacity());
-        System.out.println("numberOfStudentsLeftAfterFirstLoop: "+ numberOfStudentsLeftAfterFirstLoop);
-        System.out.println("Center 2 Capacity: " + centreList.get(1).getCentreCapacity());
-        System.out.println("expectedStudentsInSecondHub: " + expectedStudentsInSecondHub);
-        System.out.println("Center 2 Student: " + expectedStudentsInSecondHub);
-        System.out.println("Students left in waiting list : "+ waitingList.size());
-        System.out.println("isStudentLeft " + isStudentLeft);
-
     }
-
 
     @Test
     @DisplayName("add 10 trainees 5 in Csharp stream into csharp tech center expected non-sharp student will be on waiting list")
@@ -185,7 +155,7 @@ public class CentreControllerAddToCentreTest {
 
         idCount = centreController.createCentre(centreList, 10, 3,idCount,0,2);
 
-        centreController.addToCentre(waitingList,centreList,waitingList.size());
+        centreController.addToCentre(waitingList, centreList, waitingList.size(),monthlyTrainees,traineesInTraining,month);
 
         boolean isAnyStreamTwoStudentNotAdded = false;
 
@@ -201,3 +171,4 @@ public class CentreControllerAddToCentreTest {
     }
 
 }
+
